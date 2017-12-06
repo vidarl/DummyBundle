8ea0e49a2b87cbfdfc4556a0ee0d5a333f5b0e8e
Vidar Langseid <vl@ez.no>
2017-12-06 11:03:21 +0100

    Added a few more views

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace Vidarl\DummyBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -14,4 +15,26 @@ class DefaultController extends Controller
     {
         return $this->render('VidarlDummyBundle:Default:index.html.twig');
     }
+
+    /**
+     * @Route("/one_xlocationid")
+     */
+    public function oneXLocatonIdAction()
+    {
+        $response = $this->render('VidarlDummyBundle:Default:index.html.twig');
+        $response->headers->set('X-Location-Id', 'location-42');
+        return $response;
+    }
+
+    /**
+     * @Route("two_xlocationids")
+     */
+    public function twoXLocatonIdsAction()
+    {
+        $response = $this->render('VidarlDummyBundle:Default:index.html.twig');
+        $response->headers->set('X-Location-Id', 'location-42,location-43');
+        $response->headers->set('xkey', 'foobar');
+        return $response;
+    }
+
 }
