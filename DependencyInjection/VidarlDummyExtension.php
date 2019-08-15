27069e281a02e28cb31b3d03223e1d743dd9dfde
Vidar Langseid <vl@ez.no>
2019-08-15 13:43:46 +0200

    Added some custom tags

--- DependencyInjection/VidarlDummyExtension.php
+++ DependencyInjection/VidarlDummyExtension.php
@@ -6,13 +6,16 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
+use Symfony\Component\Config\Resource\FileResource;
+use Symfony\Component\Yaml\Yaml;
 
 /**
  * This is the class that loads and manages your bundle configuration.
  *
  * @link http://symfony.com/doc/current/cookbook/bundles/extension.html
  */
-class VidarlDummyExtension extends Extension
+class VidarlDummyExtension extends Extension implements PrependExtensionInterface
 {
     /**
      * {@inheritdoc}
@@ -24,5 +27,13 @@ class VidarlDummyExtension extends Extension
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+    }
+
+    public function prepend( ContainerBuilder $container )
+    {
+        $configFile = __DIR__ . '/../Resources/config/custom_tags.yml';
+        $config = Yaml::parse( file_get_contents( $configFile ) );
+        $container->prependExtensionConfig( 'ezpublish', $config );
+        $container->addResource( new FileResource( $configFile ) );
     }
 }
